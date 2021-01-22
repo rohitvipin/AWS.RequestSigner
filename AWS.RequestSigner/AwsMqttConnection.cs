2bd6ddc4f57b4b34d9dd3cde498da0ebf44bde79
rohitvipin <rohitvipin@gmail.com>
2021-01-23 01:50:34 +0530

    Add project files.

---
+++ AWS.RequestSigner/AwsMqttConnection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using AWS.RequestSigner.Signers;
+
+namespace AWS.RequestSigner
+{
+  public class AwsMqttConnection
+  {
+    public string Host { get; set; }
+
+    public string Region { get; set; }
+
+    public string AccessKey { get; set; }
+
+    public string SecretKey { get; set; }
+
+    public string SignRequestUrl()
+    {
+      var endpointBuilder = new UriBuilder("wss", Host, 443, "mqtt");
+
+      var aws4SignerForQueryParameterAuth = new AWS4SignerForQueryParameterAuth
+      {
+        EndpointUri = endpointBuilder.Uri,
+        HttpMethod = "GET",
+        Service = "iotdevicegateway",
+        Region = Region
+      };
+
+      var headers = new Dictionary<string, string>();
+
+      var authorization = aws4SignerForQueryParameterAuth.ComputeSignature(headers,
+        string.Empty,
+        AWS4SignerBase.EMPTY_BODY_SHA256,
+        AccessKey,
+        SecretKey);
+
+      var signedRequestBuilder = new UriBuilder(endpointBuilder.Uri)
+      {
+        Query = authorization
+      };
+
+      return signedRequestBuilder.Uri.AbsoluteUri;
+    }
+  }
+}
